d853083872d440291def454bac784090170f7ebb
Daniel Zunarelli <152896851+DHZuna@users.noreply.github.com>
2024-09-29 20:38:19 -0300

    add quickinfo and display
    quickinfo and display

--- dataquality.py
+++ dataquality.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+from IPython.display import display, HTML
 
 # Classe DataQuality para analisar datasets e gerar gráficos
 class DataQuality: 
@@ -9,12 +8,22 @@ class DataQuality:
  
     # Ver informações rápidas sobre a tabela:
     def quickinfo(self):
-        # shape é um atributo, não um método
         shape = self.dataframe.shape       
         null_counts = self.dataframe.isnull().sum()       
-        duplicated = self.dataframe.duplicated().sum()  # Corrigido: acesso direto ao dataframe
+        duplicated = self.dataframe.duplicated().sum()  
 
-        print("Contagem de Linhas e Colunas (Linhas, Colunas):\n", shape)  # Contagem de linhas e colunas
-        
-        print("Contagem de valores nulos por coluna:\n", null_counts)  # Contagem de nulos
-        print("Contagem de linhas duplicadas:\n", duplicated)  # Contagem de duplicados
+        #Criar dataframe unificado
+        quick_info_df = pd.DataFrame({ 
+
+            "Descrição" : ["Linhas", "Colunas", "Valores Nulos", "Linhas Duplicadas"],
+            "Contagem" : [ 
+                shape[0],
+                shape[1],
+                null_counts[null_counts > 0].sum(),
+                duplicated
+             ]
+        })
+
+# Aumentar a fonte e exibir a tabela
+        display(HTML("<h3 style='font-size: 20px;'>Quick Info</h3>"))
+        display(quick_info_df.style.set_table_attributes('style="font-size: 16px;"'))
