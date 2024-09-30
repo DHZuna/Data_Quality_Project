cddf5764c93ffa64f3fd0b8727e4b1590160eb64
Daniel Zunarelli <152896851+DHZuna@users.noreply.github.com>
2024-09-29 23:33:43 -0300

    datatypes add

--- dataquality.py
+++ dataquality.py
@@ -22,12 +22,39 @@ class DataQuality:
              ]
         })
         # Fonte maior e tabela
-        display(HTML("<h3 style='font-size: 20px;'>Quick Info</h3>"))
+        display(HTML("<h2 style='font-size: 20px;'>Quick Info</h2>"))
         display(quick_info_df.style.set_table_attributes('style="font-size: 16px;"'))
+
+    def datatypes(self):  # Inferred Data Types
+        # Mapear os tipos de dados inferidos
+        inferred_types = []
+        for col in self.dataframe.columns:
+            if self.dataframe[col].dtype == 'object':
+                unique_values = self.dataframe[col].nunique()
+                if unique_values == len(self.dataframe):
+                    inferred_types.append('unique')
+                else:
+                    inferred_types.append('categorical')
+            elif pd.api.types.is_numeric_dtype(self.dataframe[col]):
+                inferred_types.append('numeric')
+            elif pd.api.types.is_bool_dtype(self.dataframe[col]):
+                inferred_types.append('boolean')
+            else:
+                inferred_types.append('categorical')
+
+        # Criar dataframe com o nome das colunas e seus tipos inferidos
+        datatypes_df = pd.DataFrame({
+            "Nome da Coluna": self.dataframe.columns,
+            "Tipo Inferido": inferred_types
+        })
+        
+        # Exibir a tabela com fonte maior
+        display(HTML("<h2 style='font-size: 20px;'>Data Types</h2>"))
+        display(datatypes_df.style.set_table_attributes('style="font-size: 16px;"'))
 
     def firsts10(self):  # Primeiras 30 linhas
         head = self.dataframe.head(10)
 
-        display(HTML("<h3 style='font-size: 20px;'>Previews</h3>"))
-        display(HTML("<h2 style='font-size: 20px;'>Primeiras 10 Linhas</h2>"))
+        display(HTML("<h2 style='font-size: 20px;'>Data Preview</h2>"))
+        display(HTML("<h3 style='font-size: 20px;'>As 10 primeiras linhas: </h3>"))
         display(head.style.set_table_attributes('style="font-size: 16px;"'))
